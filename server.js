a85287e828566f34884fac8c52869866b95b9e3d
FX-Wood <fxwood2@gmail.com>
2019-02-21 14:21:45 -0800

    updated environmental variable support

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const request = require('request');
+require('dotenv').config();
+const fs = require('fs');
 
 const app = express();
 
@@ -8,14 +10,28 @@ app.use(express.static('static'));
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
-    request('https://www.google.com', function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            res.send(body)
+    let qs = {
+        s: 'star wars',
+        apikey: process.env.API_KEY
+    }
+    request({
+        url: 'https://www.omdbapi.com',
+        qs: qs
+    }, function (error, response, body) {
+        console.log('trying')
+        if (!error && response.statusCode == 200) {
+            let dataObj = JSON.parse(body);
+            res.render('results', {results: dataObj.Search});
+        } else {
+            console.log(response.statusCode)
+            console.log('fail')
         }
-    })
-})
+    });
+});
 
-app.listen(3000, function() {
-    console.log('omdb-api is running');
-    console.log('listening on 3000');
+app.listen(process.env.PORT || 3000
+    , function() {
+        console.log('omdb-api is running');
+        console.log('listening on: port', process.env.PORT || 3000);
+        console.log('Your api key:', process.env.API_KEY)
 })
